05b06ff6614d3ded7d8ae5f72a4068a66152bf81
Michael Chang <mchang0926@gmail.com>
2020-05-01 15:30:58 -0700

    update our implementation of how we connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require('express')
 const cors = require('cors')
-const monk = require('monk')
+const express = require('express')
+const mongoose = require('mongoose')
+const Woof = require('./models/Woofs')
+const app = express()
+
 // Prevent spans and swear words
 const Filter = require('bad-words')
 const rateLimit = require('express-rate-limit')
 
-const app = express()
+require('dotenv/config')
 
-const db = monk(process.env.MONGO_URI || 'localhost/woofer')
-const woofs = db.get('woofs')
 const filter = new Filter()
 
 app.use(cors())
@@ -21,12 +22,13 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/woofs', (req, res) => {
-    woofs
-    .find()
-    .then( woofs => {
+app.get('/woofs', async (req, res) => {
+    try {
+        const woofs = await Woof.find()
         res.json(woofs)
-    })
+    } catch (err) {
+        res.json({message: err})
+    }
 })
 
 function isValidWoof(woof)
@@ -41,20 +43,21 @@ app.use(rateLimit({
     max: 1
 }))
 
-app.post('/woofs', (req, res) => {
+app.post('/woofs', async (req, res) => {
     // console.log(req.body)
     if (isValidWoof(req.body)) 
     {
-        const woof = { 
+        const woof = new Woof({ 
             name: filter.clean(req.body.name.toString()),
             content: filter.clean(req.body.content.toString()),
-            created: new Date()
+            // created: new Date()
+        })
+        try {
+            const savedWoof = await woof.save()
+            res.json(savedWoof)
+        } catch(err) {
+            res.json({message: err})
         }
-        woofs
-            .insert(woof)
-            .then(createdWoof => {
-                res.json(createdWoof)
-            })
     } 
     else 
     {
@@ -65,6 +68,13 @@ app.post('/woofs', (req, res) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log('Listening on http://localhost:5000')
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+     () => console.log('connected to DB!!'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
